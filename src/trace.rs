a6ff8773731bbf51cb26e9bc6ec35cba27899b37
Rafal Jasinski <rajs@softwaremind.com>
2026-02-11 17:27:42 +0100

    Changed Trace initialization

--- src/trace.rs
+++ src/trace.rs
@@ -16,15 +16,14 @@ impl Trace {
     pub fn new(
         size: u32,
         timestamp: u32,
-        message_level: u32,
-        message_code: u32,
-        argument: Option<u32>,
+        message_level: message::Level,
+        message_code: message::Code,
     ) -> Self {
         Self {
             size: size,
             timestamp: timestamp,
-            message_level: message::Level::from(message_level),
-            message_code: message::Code::new(message_code, argument),
+            message_level: message_level,
+            message_code: message_code,
         }
     }
 }
@@ -51,28 +50,48 @@ mod test {
             message_level: message::Level::Info,
             message_code: message::Code::new(0x1234, None),
         };
-        let new_trace = Trace::new(0x10, 0x1234, 0x0, 0x1234, None);
+        let new_trace = Trace::new(
+            0x10,
+            0x1234,
+            message::Level::Info,
+            message::Code::new(0x1234, None),
+        );
         assert_eq!(correct_trace, new_trace);
     }
 
     #[test]
     fn trace_fmt_test() {
         {
-            let simple_trace = Trace::new(0x10, 0x1234, 0x0, 0x1234, None);
+            let simple_trace = Trace::new(
+                0x10,
+                0x1234,
+                message::Level::Info,
+                message::Code::new(0x1234, None),
+            );
             let correct_fmt = String::from("[INFO] 0x1234 - Unknown code!");
             let created_fmt = format!("{}", simple_trace);
             assert_eq!(correct_fmt, created_fmt);
         }
 
         {
-            let simple_trace = Trace::new(0x10, 0x1234, 0x0, 0x1234, Some(0x1234));
+            let simple_trace = Trace::new(
+                0x10,
+                0x1234,
+                message::Level::Info,
+                message::Code::new(0x1234, Some(0x1234)),
+            );
             let correct_fmt = String::from("[INFO] 0x1234 - Unknown code! (0x1234)");
             let created_fmt = format!("{}", simple_trace);
             assert_eq!(correct_fmt, created_fmt);
         }
 
         {
-            let simple_trace = Trace::new(0x10, 0x1234, 0x0, 0x155, None);
+            let simple_trace = Trace::new(
+                0x10,
+                0x1234,
+                message::Level::Info,
+                message::Code::new(0x155, None),
+            );
             let correct_fmt = String::from("[INFO] 0x1234 - BOOTCORE - Boot ROM version");
             let created_fmt = format!("{}", simple_trace);
             assert_eq!(correct_fmt, created_fmt);
